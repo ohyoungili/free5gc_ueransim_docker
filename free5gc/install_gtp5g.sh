943a2bafa773ed11353ffe85129b2c347d8350f8
ohyoungili <88310796+ohyoungili@users.noreply.github.com>
2021-08-19 11:03:52 +0900

    Update install_gtp5g.sh

--- free5gc/install_gtp5g.sh
+++ free5gc/install_gtp5g.sh
@@ -6,6 +6,7 @@
 
 # As noted above, the GTP kernel module used by the UPF requires that you use Linux kernel version 5.0.0-23-generic. To verify your version:
 #uname -r
+# 5.8.0-63-generic 
 
 git clone -b v0.2.1 https://github.com/free5gc/gtp5g.git
 cd gtp5g
